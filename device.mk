8b09b4043c7223b98b4cab208fedfc19ee861dc0
Ari Börde Kröyer <ari.kroyer@online.no>
2020-09-12 21:44:43 +0200

    Add ofono override

--- device.mk
+++ device.mk
@@ -59,6 +59,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/ubuntu/rsyslog.conf:system/halium/etc/rsyslog.conf \
     $(LOCAL_PATH)/ubuntu/timekeeper.conf:system/halium/etc/init/timekeeper.conf \
     $(LOCAL_PATH)/ubuntu/touch.pa:system/halium/etc/pulse/touch.pa \
+    $(LOCAL_PATH)/ubuntu/ofono.override:system/halium/etc/ofono.override \    
     $(LOCAL_PATH)/ubuntu/usr.bin.media-hub-server:system/halium/etc/apparmor.d/local/usr.bin.media-hub-server
     
 
